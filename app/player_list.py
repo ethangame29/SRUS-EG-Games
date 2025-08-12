28cf69fdd74f7f98fa3e61eb50a1ed586bd998e5
ETHAN GAME <GAMEET@tdm.local>
2025-08-12 12:58:19 +0800

    Add appendEnd to player_list.py and respective unit test

--- app/player_list.py
+++ app/player_list.py
@@ -25,3 +25,12 @@ class PlayerList:
             newNode = player_node.PlayerNode(player.Player(id, name))
             self.__head = newNode
             self.__tail = newNode
+
+    def appendEnd(self, id, name):
+        if self.__head is not None:
+            newNode = player_node.PlayerNode(player.Player(id, name))
+            self.__tail.setPrevNode(newNode)
+            newNode.setNextNode(self.__tail)
+            self.__tail = newNode
+        else:
+            self.append(id, name)
--- test/player_list_test.py
+++ test/player_list_test.py
@@ -19,3 +19,13 @@ class PlayerListTest(unittest.TestCase):
         self.assertEqual(playerList.head.prevNode.key, 1234)
         self.assertEqual(playerList.head.prevNode.nextNode.key, 3)
         self.assertEqual(playerList.head.prevNode, playerList.tail)
+
+    def test_append_to_end_of_list(self):
+        playerlist = PlayerList()
+        playerlist.appendEnd(1234, "Joe")
+        self.assertEqual(playerlist.tail.key, 1234)
+        playerlist.appendEnd(3, "Ethan")
+        self.assertEqual(playerlist.tail.key, 3)
+        self.assertEqual(playerlist.head.key, 1234)
+        self.assertEqual(playerlist.head.prevNode, playerlist.tail)
+        self.assertEqual(playerlist.tail.nextNode, playerlist.head)
