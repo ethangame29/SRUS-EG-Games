b86225a189dccf7bb48b0e07452532895edbc330
ETHAN GAME <GAMEET@tdm.local>
2025-08-19 15:19:04 +0800

    Add getitem method with custom error class

--- app/player_hash_map.py
+++ app/player_hash_map.py
@@ -1,6 +1,10 @@
 from app.player_list import PlayerList
 from app.player import Player
 
+class NotFoundError(Exception):
+    def __init__(self, message):
+        self.message = message
+
 class PlayerHashMap:
     SIZE: int = 10
 
@@ -18,8 +22,8 @@ class PlayerHashMap:
 
     def __setitem__(self, key: str, name: str) -> None:
         player_found = False
-        playerList = self.hashmap[self.get_index(key)]
-        current_node = playerList.head
+        player_list = self.hashmap[self.get_index(key)]
+        current_node = player_list.head
         while current_node is not None:
             if current_node.player.uid == key:
                 current_node.player.set_name(name)
@@ -27,4 +31,16 @@ class PlayerHashMap:
             current_node = current_node.prevNode
 
         if not player_found:
-            playerList.append(key, name)
+            player_list.append(key, name)
+
+    def __getitem__(self, key: str) -> None:
+        player_found = False
+        player_list = self.hashmap[self.get_index(key)]
+        current_node = player_list.head
+        while current_node is not None:
+            if current_node.player.uid == key:
+                player_found = True
+            current_node = current_node.prevNode
+
+        if not player_found:
+            raise NotFoundError("Player not found")
