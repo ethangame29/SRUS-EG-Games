cad70868864e8552df3a3594397fec9048aa4491
ETHAN GAME <GAMEET@tdm.local>
2025-09-02 14:26:11 +0800

    Add docstring to player_hash_map.py

--- app/player_hash_map.py
+++ app/player_hash_map.py
@@ -3,11 +3,30 @@ from app.player import Player
 
 
 class NotFoundError(Exception):
+    """
+    Throws NotFoundError.
+
+    Args:
+        message (str): Error Message.
+
+    Returns:
+        None
+    """
     def __init__(self, message):
         self.message = message
 
 
 def player_locator(player_list, key):
+    """
+    Locates a Player in a HashMap.
+
+    Args:
+        player_list (PlayerList): The Player List.
+        key (str): Player Key.
+
+    Returns:
+        None or current_node
+    """
     current_node = None
 
     if player_list.head is not None:
@@ -22,6 +41,9 @@ def player_locator(player_list, key):
 
 
 class PlayerHashMap:
+    """
+    Hash Map for storing Players
+    """
     SIZE: int = 10
 
     def __init__(self):
@@ -30,6 +52,9 @@ class PlayerHashMap:
             self.hashmap.append(PlayerList())
 
     def display(self):
+        """
+        Displays the Hash Map in console
+        """
         index = 0
         for player_list in self.hashmap:
             if len(player_list) >= 1:
@@ -39,12 +64,28 @@ class PlayerHashMap:
             index += 1
 
     def get_index(self, key: str | Player) -> int:
+        """
+        Gets index of a Player through a hash.
+
+        Args:
+            key (str | Player): The player key.
+
+        Returns:
+            int: Hash Value
+        """
         if isinstance(key, Player):
             return hash(key) % self.SIZE
         else:
             return Player.hash_function(key) % self.SIZE
 
     def __setitem__(self, key: str, name: str) -> None:
+        """
+        Set a player in Hash Map.
+
+        Args:
+            key (str): The player key.
+            name (str): The player name.
+        """
         player_list = self.hashmap[self.get_index(key)]
         player = player_locator(player_list, key)
 
@@ -54,6 +95,15 @@ class PlayerHashMap:
             player.player.name = name
 
     def __getitem__(self, key: str):
+        """
+        Get Player from Hash Map.
+
+        Args:
+            key (str): The player key.
+
+        Returns:
+            player
+        """
         player_list = self.hashmap[self.get_index(key)]
         player = player_locator(player_list, key)
 
@@ -63,6 +113,12 @@ class PlayerHashMap:
             raise NotFoundError("Player not found")
 
     def __delitem__(self, key: str):
+        """
+        Deletes Player from Hash Map.
+
+        Args:
+            key (str): The Player Key
+        """
         player_list = self.hashmap[self.get_index(key)]
         player = player_locator(player_list, key)
 
@@ -72,6 +128,12 @@ class PlayerHashMap:
             raise NotFoundError("Player not found")
 
     def __len__(self):
+        """
+        Grabs Length of Hash Map
+
+        returns:
+            int: length
+        """
         length = 0
         for player_list in self.hashmap:
             length = length + len(player_list)
