ca82700ec6ba38726ea635571c02c0d4c562ffe4
ETHAN GAME <GAMEET@tdm.local>
2025-09-23 15:59:41 +0800

    add insert method

--- app/player_bst.py
+++ app/player_bst.py
@@ -1,3 +1,6 @@
+from player_bnode import PlayerBNode
+
+
 class PlayerBST:
 
     def __init__(self):
@@ -6,3 +9,23 @@ class PlayerBST:
     @property
     def root(self):
         return self._root
+
+    def insert(self, player, location):
+        if self._root is None:
+            self._root = PlayerBNode(player)
+            return
+
+        if location is None:
+            location = self._root
+            # return
+
+        if location.player.name < player.name:
+            if location.left is None:
+                location.left = PlayerBNode(player)
+            else:
+                self.insert(player, location.left)
+        else:
+            if location.right is None:
+                location.right = PlayerBNode(player)
+            else:
+                self.insert(player, location.right)
